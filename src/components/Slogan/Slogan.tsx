7c5d5aa413b944b6cfffd136686f110e84cb95eb
Erekle98 <e.lomsadze1@gmail.com>
2023-09-25 20:36:51 +0400

    modify slogan component

--- src/components/Slogan/Slogan.tsx
+++ src/components/Slogan/Slogan.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Image from "next/image";
+
 import SloganStyles from "@/styles/components/Slogan.module.scss";
+
+import sloganImage from "@/public/images/shutterstock@2x.png";
 
 const Slogan: React.FC = () => {
   return (
     <div className={SloganStyles.slogan}>
+      <Image src={sloganImage} alt="slogan" priority />
       <h1>გაყიდე და იყიდე დომენი მარტივად</h1>
     </div>
   );
